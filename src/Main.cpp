265d758e823cc44d13dedc7886575a513ab81933
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-05-29 16:23:56 -0300

    Initial commit

---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <controller_tuning_widgets/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+    controller_tuning_widgets::DummyClass dummyClass;
+    dummyClass.welcome();
+
+    return 0;
+}
